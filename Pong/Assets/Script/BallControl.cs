df091331fb16338fd8c53ef3082404a12abde767
Dany Zaky <danyzakydany123@gmail.com>
2021-09-07 19:05:45 +0700

    add build file and FINISH

--- Pong/Assets/Script/BallControl.cs
+++ Pong/Assets/Script/BallControl.cs
@@ -13,6 +13,8 @@ public class BallControl : MonoBehaviour
     // Titik asal lintasan bola saat ini
     private Vector2 trajectoryOrigin;
 
+    public float ballSpeed;
+
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
@@ -23,16 +25,16 @@ public class BallControl : MonoBehaviour
         trajectoryOrigin = transform.position;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         //Jika nilai rigidbody2d.velocity.magnitude (BallSpeed) tidak sama dengan 10, maka nilai rigidbody2d.velocity.magnitude (BallSpeed) nilainya 10
 
         //Memanggil normalisasi agak memakan waktu (perlu akar kuadrat) tetapi Anda tidak akan melihat masalah apa pun jika Anda tidak menjalankan ribuan
         //instance objek ini (dan saya kira fungsi ClampMagnitude tetap membutuhkan akar kuadrat dan akan tidak lebih efisien)
 
-        if (rigidBody2D.velocity.sqrMagnitude != 10f * 10f)
+        if (rigidBody2D.velocity.sqrMagnitude != ballSpeed * ballSpeed)
         {
-            rigidBody2D.velocity = rigidBody2D.velocity.normalized * 10f;
+            rigidBody2D.velocity = rigidBody2D.velocity.normalized * ballSpeed;
         }
     }
     void ResetBall()
@@ -59,7 +61,7 @@ public class BallControl : MonoBehaviour
         }
 
         //Tugas: Saya memaksakan nilai dari rigidbody2d.velocity.magnitude (BallSpeed) menjadi konstan dengan menerapkan pada method
-        // void FixedUpdate
+        // void Update
     }
 
     void RestartGame()
